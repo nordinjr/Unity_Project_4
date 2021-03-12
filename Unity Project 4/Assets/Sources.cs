76572a15a2490f0af2508ace1237036b28feacb4
Brandon <wendellcasey1999@gmail.com>
2021-03-11 20:24:05 -0600

    Added links to sources

--- Unity Project 4/Assets/Sources.cs
+++ Unity Project 4/Assets/Sources.cs
@@ -1,1 +1,3 @@
 // heart and bar : https://github.com/Brackeys/Health-Bar/blob/master/Health%20Bar/Assets/Sprites/Bar.png
+// Jellyfish : https://www.hiddenone-sprites.com/resource-blog/sv-battler-sprite-jamming-jellyfish
+// Dolphin: https://opengameart.org/content/dolphin-baby-and-ocean-tiles
